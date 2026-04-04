fe623f83360c12821bd200415041c143ae219d11
isoev254-bit <isoev254@gmail.com>
2026-04-04 13:31:11 +0500

    Create index.ts

---
+++ backend/src/modules/auth/index.ts
@@ -0,0 +1,7 @@
+// modules/auth/index.ts - Экспорти ҳамаи қисмҳои модули аутентификатсия
+export { AuthController } from './auth.controller';
+export { AuthService } from './auth.service';
+export { AuthRepository } from './auth.repository';
+export { AuthValidator } from './auth.validator';
+export { default as authRoutes } from './auth.routes';
+export * from './auth.types';
