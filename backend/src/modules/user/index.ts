90a53bf110cf9143cc04d55fc23e63e20b55acfa
isoev254-bit <isoev254@gmail.com>
2026-04-08 09:51:14 +0500

    Create index.ts

---
+++ backend/src/modules/user/index.ts
@@ -0,0 +1,6 @@
+// modules/user/index.ts - Экспорти ҳамаи қисмҳои модули корбар
+export { UserController } from './user.controller';
+export { UserService } from './user.service';
+export { UserRepository } from './user.repository';
+export { default as userRoutes } from './user.routes';
+export * from './user.types';
