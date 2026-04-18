a827e677621ae70b0f3e0ddacd47b38b3702b10e
isoev254-bit <isoev254@gmail.com>
2026-04-18 09:34:05 +0500

    Create index.ts

---
+++ frontend/client/src/pages/OrderHistory/index.ts
@@ -0,0 +1,9 @@
+// Tojrason/frontend/client/src/pages/OrderHistory/index.ts
+/*
+ * © 2024 Тоҷрасон. Ҳамаи ҳуқуқҳо маҳфузанд.
+ * Ин файл қисми нармафзори "Тоҷрасон" мебошад.
+ * Истифодаи ғайриқонунии ин файл ё қисмҳои он тибқи қонунгузории Ҷумҳурии Тоҷикистон таъқиб карда мешавад.
+ */
+
+export { default as OrderHistory } from './OrderHistory';
+export { default as OrderDetails } from './OrderDetails';
