467a6a0b6a1e5a7e060ef522996213d23f081a93
isoev254-bit <isoev254@gmail.com>
2026-04-18 09:42:53 +0500

    Create index.ts

---
+++ frontend/client/src/pages/Payment/index.ts
@@ -0,0 +1,10 @@
+// Tojrason/frontend/client/src/pages/Payment/index.ts
+/*
+ * © 2024 Тоҷрасон. Ҳамаи ҳуқуқҳо маҳфузанд.
+ * Ин файл қисми нармафзори "Тоҷрасон" мебошад.
+ * Истифодаи ғайриқонунии ин файл ё қисмҳои он тибқи қонунгузории Ҷумҳурии Тоҷикистон таъқиб карда мешавад.
+ */
+
+export { default as Payment } from './Payment';
+export { default as PaymentSuccess } from './PaymentSuccess';
+export { default as PaymentCancel } from './PaymentCancel';
