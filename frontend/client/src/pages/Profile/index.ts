4a75e276f32d3bbcdcd9d7b8ce69df83ca65ad06
isoev254-bit <isoev254@gmail.com>
2026-04-18 09:51:05 +0500

    Create index.ts

---
+++ frontend/client/src/pages/Profile/index.ts
@@ -0,0 +1,10 @@
+// Tojrason/frontend/client/src/pages/Profile/index.ts
+/*
+ * © 2024 Тоҷрасон. Ҳамаи ҳуқуқҳо маҳфузанд.
+ * Ин файл қисми нармафзори "Тоҷрасон" мебошад.
+ * Истифодаи ғайриқонунии ин файл ё қисмҳои он тибқи қонунгузории Ҷумҳурии Тоҷикистон таъқиб карда мешавад.
+ */
+
+export { default as Profile } from './Profile';
+export { default as EditProfile } from './EditProfile';
+export { default as ChangePassword } from './ChangePassword';
