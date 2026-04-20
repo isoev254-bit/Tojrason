ed4631a2290ec2b6094c72d7cf786f28a319af30
isoev254-bit <isoev254@gmail.com>
2026-04-20 10:46:38 +0500

    Create index.ts

---
+++ frontend/courier/src/pages/Orders/index.ts
@@ -0,0 +1,10 @@
+// Tojrason/frontend/courier/src/pages/Orders/index.ts
+/*
+ * © 2024 Тоҷрасон. Ҳамаи ҳуқуқҳо маҳфузанд.
+ * Ин файл қисми нармафзори "Тоҷрасон" мебошад.
+ * Истифодаи ғайриқонунии ин файл ё қисмҳои он тибқи қонунгузории Ҷумҳурии Тоҷикистон таъқиб карда мешавад.
+ */
+
+export { default as Orders } from './Orders';
+export { default as AvailableOrders } from './AvailableOrders';
+export { default as MyOrders } from './MyOrders';
