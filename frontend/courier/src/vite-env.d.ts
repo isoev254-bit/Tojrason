ee2e7e035122e76aef89e91362733fafd8c19f00
isoev254-bit <isoev254@gmail.com>
2026-04-20 12:52:46 +0500

    Create vite-env.d.ts

---
+++ frontend/courier/src/vite-env.d.ts
@@ -0,0 +1,136 @@
+// Tojrason/frontend/courier/src/vite-env.d.ts
+/*
+ * © 2024 Тоҷрасон. Ҳамаи ҳуқуқҳо маҳфузанд.
+ * Ин файл қисми нармафзори "Тоҷрасон" мебошад.
+ * Истифодаи ғайриқонунии ин файл ё қисмҳои он тибқи қонунгузории Ҷумҳурии Тоҷикистон таъқиб карда мешавад.
+ */
+
+/// <reference types="vite/client" />
+
+/**
+ * Тағйирёбандаҳои муҳит барои Vite (версияи курйер)
+ */
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+  readonly VITE_WS_URL: string;
+  readonly VITE_APP_NAME: string;
+  readonly VITE_APP_VERSION: string;
+  readonly VITE_ENABLE_MOCK: string;
+  readonly VITE_GOOGLE_MAPS_API_KEY: string;
+  readonly VITE_SENTRY_DSN: string;
+  readonly VITE_APP_ENV: 'development' | 'staging' | 'production';
+  readonly VITE_COURIER_APP: string;
+}
+
+/**
+ * Интерфейси ImportMeta барои дастрасӣ ба тағйирёбандаҳои муҳит
+ */
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+/**
+ * Намудҳо барои воридоти тасвирҳо
+ */
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpeg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.gif' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.webp' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.svg' {
+  import * as React from 'react';
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+
+/**
+ * Намудҳо барои воридоти услубҳои CSS модулӣ
+ */
+declare module '*.module.css' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module '*.module.scss' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module '*.module.sass' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module '*.module.less' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+/**
+ * Намудҳо барои воридоти файлҳои дигар
+ */
+declare module '*.json' {
+  const value: any;
+  export default value;
+}
+
+declare module '*.mp4' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.webm' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.mp3' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.wav' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.ogg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.pdf' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.woff' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.woff2' {
+  const src: string;
+  export default src;
+}
