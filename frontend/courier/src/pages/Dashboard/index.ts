d2879ebc911346f9e7b90e7bd9f8b1bf106f4005
isoev254-bit <isoev254@gmail.com>
2026-04-20 10:35:33 +0500

    Create index.ts

---
+++ frontend/courier/src/pages/Dashboard/index.ts
@@ -0,0 +1,9 @@
+// Tojrason/frontend/courier/src/pages/Dashboard/index.ts
+/*
+ * © 2024 Тоҷрасон. Ҳамаи ҳуқуқҳо маҳфузанд.
+ * Ин файл қисми нармафзори "Тоҷрасон" мебошад.
+ * Истифодаи ғайриқонунии ин файл ё қисмҳои он тибқи қонунгузории Ҷумҳурии Тоҷикистон таъқиб карда мешавад.
+ */
+
+export { default } from './Dashboard';
+export { default as Dashboard } from './Dashboard';
