c92373dd6291268025bfc3cc8bb18e5e65f61168
isoev254-bit <isoev254@gmail.com>
2026-04-20 10:56:11 +0500

    Create index.ts

---
+++ frontend/courier/src/pages/CurrentOrder/index.ts
@@ -0,0 +1,10 @@
+// Tojrason/frontend/courier/src/pages/CurrentOrder/index.ts
+/*
+ * © 2024 Тоҷрасон. Ҳамаи ҳуқуқҳо маҳфузанд.
+ * Ин файл қисми нармафзори "Тоҷрасон" мебошад.
+ * Истифодаи ғайриқонунии ин файл ё қисмҳои он тибқи қонунгузории Ҷумҳурии Тоҷикистон таъқиб карда мешавад.
+ */
+
+export { default as CurrentOrder } from './CurrentOrder';
+export { default as OrderSteps } from './OrderSteps';
+export { default as ContactClient } from './ContactClient';
