e07d0f6946e4f572249906ca8780a97f9aa646d4
isoev254-bit <isoev254@gmail.com>
2026-04-21 18:53:35 +0500

    Create index.ts

---
+++ frontend/shared/components/Input/index.ts
@@ -0,0 +1,10 @@
+// Tojrason/frontend/shared/components/Input/index.ts
+/*
+ * © 2024 Тоҷрасон. Ҳамаи ҳуқуқҳо маҳфузанд.
+ * Ин файл қисми нармафзори "Тоҷрасон" мебошад.
+ * Истифодаи ғайриқонунии ин файл ё қисмҳои он тибқи қонунгузории Ҷумҳурии Тоҷикистон таъқиб карда мешавад.
+ */
+
+export { default } from './Input';
+export { default as Input } from './Input';
+export type { InputProps, InputVariant, InputSize, LabelPosition } from './Input';
