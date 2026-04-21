8f0934f7276c51204e6f80815c1078250200d096
isoev254-bit <isoev254@gmail.com>
2026-04-21 19:10:09 +0500

    Create index.ts

---
+++ frontend/shared/components/Badge/index.ts
@@ -0,0 +1,10 @@
+// Tojrason/frontend/shared/components/Badge/index.ts
+/*
+ * © 2024 Тоҷрасон. Ҳамаи ҳуқуқҳо маҳфузанд.
+ * Ин файл қисми нармафзори "Тоҷрасон" мебошад.
+ * Истифодаи ғайриқонунии ин файл ё қисмҳои он тибқи қонунгузории Ҷумҳурии Тоҷикистон таъқиб карда мешавад.
+ */
+
+export { default } from './Badge';
+export { default as Badge } from './Badge';
+export type { BadgeProps, BadgeVariant, BadgeSize, BadgeType } from './Badge';
