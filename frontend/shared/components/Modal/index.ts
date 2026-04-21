d774198555053bed7755125c7c963032c0d0897c
isoev254-bit <isoev254@gmail.com>
2026-04-21 18:58:09 +0500

    Create index.ts

---
+++ frontend/shared/components/Modal/index.ts
@@ -0,0 +1,10 @@
+// Tojrason/frontend/shared/components/Modal/index.ts
+/*
+ * © 2024 Тоҷрасон. Ҳамаи ҳуқуқҳо маҳфузанд.
+ * Ин файл қисми нармафзори "Тоҷрасон" мебошад.
+ * Истифодаи ғайриқонунии ин файл ё қисмҳои он тибқи қонунгузории Ҷумҳурии Тоҷикистон таъқиб карда мешавад.
+ */
+
+export { default } from './Modal';
+export { default as Modal } from './Modal';
+export type { ModalProps, ModalSize, ModalPosition } from './Modal';
