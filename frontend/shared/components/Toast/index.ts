9a34c1670f20e20c5a7981ccd0777fb73e22d87f
isoev254-bit <isoev254@gmail.com>
2026-04-21 19:14:46 +0500

    Create index.ts

---
+++ frontend/shared/components/Toast/index.ts
@@ -0,0 +1,21 @@
+// Tojrason/frontend/shared/components/Toast/index.ts
+/*
+ * © 2024 Тоҷрасон. Ҳамаи ҳуқуқҳо маҳфузанд.
+ * Ин файл қисми нармафзори "Тоҷрасон" мебошад.
+ * Истифодаи ғайриқонунии ин файл ё қисмҳои он тибқи қонунгузории Ҷумҳурии Тоҷикистон таъқиб карда мешавад.
+ */
+
+export { 
+  default,
+  toastSuccess,
+  toastError,
+  toastWarning,
+  toastInfo,
+  toastLoading,
+  dismissToast,
+  updateToast,
+  ToastProvider,
+  toastUtils
+} from './Toast';
+
+export type { ToastProps, ToastType, ToastPosition } from './Toast';
