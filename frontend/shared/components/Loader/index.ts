4994f8493a73f61369cf7998b337272b6fc3b332
isoev254-bit <isoev254@gmail.com>
2026-04-21 19:02:00 +0500

    Create index.ts

---
+++ frontend/shared/components/Loader/index.ts
@@ -0,0 +1,10 @@
+// Tojrason/frontend/shared/components/Loader/index.ts
+/*
+ * © 2024 Тоҷрасон. Ҳамаи ҳуқуқҳо маҳфузанд.
+ * Ин файл қисми нармафзори "Тоҷрасон" мебошад.
+ * Истифодаи ғайриқонунии ин файл ё қисмҳои он тибқи қонунгузории Ҷумҳурии Тоҷикистон таъқиб карда мешавад.
+ */
+
+export { default } from './Loader';
+export { default as Loader } from './Loader';
+export type { LoaderProps, LoaderSize, LoaderVariant, LoaderColor } from './Loader';
